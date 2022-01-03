f8b7ba97487b72975427683f6f9de6bcb1d424dc
every3743 <every3743@gmail.com>
2022-01-03 11:55:53 +0900

    commit

---
+++ src/javaCoding/특정문자뒤집기.java
@@ -0,0 +1,43 @@
+package javaCoding;
+
+import java.util.Scanner;
+
+/**
+ * 설명 : 영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고, 특수 문자는 자기 자리에 그대로 있는 문자열을 만들어
+ * 출력하는 프로그램을 작성하세요.
+ * 
+ * 입력 : 첫 줄에 길이가 100을 넘지 않는 문자열이 주어집니다.
+ * 
+ * 출력 : 첫 줄에 알파벳만 뒤집힌 문자열을 출력합니다.
+ * 
+ * 예시 입력 : a#b!GE*T@S 예시 출력 : S#T!EG*b@a
+ */
+public class 특정문자뒤집기 {
+	public String solution(String str) {
+		String answer;
+		char[] s = str.toCharArray();
+		int lt = 0, rt = str.length() - 1;
+		while (lt < rt) {
+			if (!Character.isAlphabetic(s[lt]))
+				lt++;
+			else if (!Character.isAlphabetic(s[rt]))
+				rt--;
+			else {
+				char tmp = s[lt];
+				s[lt] = s[rt];
+				s[rt] = tmp;
+				lt++;
+				rt--;
+			}
+		}
+		answer = String.valueOf(s);
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		특정문자뒤집기 T = new 특정문자뒤집기();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
+	}
+}
