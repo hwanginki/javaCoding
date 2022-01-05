1c5da4d1b305132f7a6a3b3fa610774f45d6bf66
every3743 <every3743@gmail.com>
2022-01-05 16:38:57 +0900

    큰 수 출력하기

--- src/array/큰수출력하기.java
+++ src/array/큰수출력하기.java
@@ -14,7 +14,7 @@ public class 큰수출력하기 {
 	public ArrayList<Integer> solution(int n, int[] arr) {
 		ArrayList<Integer> answer = new ArrayList<>();
 		answer.add(arr[0]);
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i < n; i++) {
 			if (arr[i] > arr[i - 1]) {
 				answer.add(arr[i]);
 			}
