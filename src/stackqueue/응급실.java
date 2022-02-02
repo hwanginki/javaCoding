ffdf61f83e14b2c5a72f8c33c90f9ff3cc73e3a4
every3743 <every3743@gmail.com>
2022-02-02 15:12:39 +0900

    응급실

--- src/stackqueue/응급실.java
+++ src/stackqueue/응급실.java
@@ -18,15 +18,13 @@ public class 응급실 {
 		int answer = 0;
 		Queue<Person> q = new LinkedList<>();
 		for (int i = 0; i < n; i++) {
-			q.offer(new Person(i, arr[i]));
+			q.offer(new Person(i, arr[i])); // offer 메서드 이용해 요소를 추가함
 		}
 		
-		
-		System.out.println(">>>>>>>>" + q.size());
-		
-		while (!q.isEmpty()) {
+		while (!q.isEmpty()) { // 요소가 비어지지 않을 때
 			Person tmp = q.poll();
 			for (Person x : q) {
+				// x가 tmp보다 크면 tmp 요소 추가하고 tmp에 null로 대입
 				if (x.priority > tmp.priority) {
 					q.offer(tmp);
 					tmp = null;
